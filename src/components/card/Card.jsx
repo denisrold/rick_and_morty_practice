2478025e9b6e54bba65923b9a1d841b3285e9421
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-01-11 09:49:56 -0300

    cards

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,9 +2,9 @@ import styles from"./Card.module.css"
 export default function Card(props) {
    return (
       <div className={styles.container}>
-         <div className={styles.buttoncontain}>
+        
             <button onClick={props.onClose}>X</button>
-         </div>   
+          
             <h2 className={styles.name} >{props.name}</h2>
             <img className={styles.imagen} src={props.image} alt={props.name}></img>
             <hr></hr>
